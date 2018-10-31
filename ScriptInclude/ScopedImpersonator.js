71116c7bc60b9919a22cde35f3b5cbdd27c64818
Federico Borgnia <borgniaf@amazon.com>
2018-10-31 20:13:20 +0000

    refactor to only impersonate the aws user instead of a generic impersonator function

--- ScriptInclude/ScopedImpersonator.js
+++ ScriptInclude/ScopedImpersonator.js
@@ -1,8 +1,15 @@
 var ScopedImpersonator = Class.create();
 ScopedImpersonator.prototype = {
     initialize: function(){},
-    impersonate: function(user) {
-      return gs.getSession().impersonate(user);
+    impersonate_aws_user: function() {
+    var user = new GlideRecord('sys_user');
+    user.addQuery('user_name', gs.getProperty("x_195647_aws_.Config.AWS.username"));
+    user.query();
+    if (user.hasNext()) {
+      user.next();
+      gs.info("impersonating " + user.user_name);
+      return gs.getSession().impersonate(user.sys_id);
+    }
     },
 
     type: 'ScopedImpersonator'
